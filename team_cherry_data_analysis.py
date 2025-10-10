497804dc968674fdf2c70a59f3e55e04686a3db1
mhoover734 <mhoover734@phm.k12.in.us>
2025-10-10 09:51:37 -0400

    Final
    I'll add the Slideshow link once I'm done with it

--- team_cherry_data_analysis.py
+++ team_cherry_data_analysis.py
@@ -34,6 +34,28 @@ print(hk_data.sort_values(by="HK hours", ascending=False).head(5))
 print(f"\n{lineBreak}\n\nTop sleep quality in HK Dataframe\n")
 print(hk_data[hk_data["HK sleep"]=='Greatly Increased'])
 
+# Scatterplot of HK Sleep Change vs. HK Hours
+for rating, group in hk_data.groupby('HK sleep #'):
+	plt.scatter([rating]*len(group['HK hours']), group['HK hours'], label=str(rating))
+plt.title('HK Hours for Sleep Change')
+plt.xlabel('Sleep Change')
+plt.ylabel('HK Rating')
+plt.show()
+
+# Pie chart of HK Ratings
+sizes = hk_data['HK rating'].value_counts().sort_index() / hk_data['HK rating'].value_counts().sum() * 100
+sizes.plot(kind='pie', labels=sizes.index, autopct='%1.1f%%')
+plt.title('HK Rating Spread')
+plt.ylabel('HK Rating')
+plt.show()
+
+# Histogram of spread of HK Hours
+df['HK hours'].plot(kind='hist', bins = 20, edgecolor='Black')
+plt.title('HK Hour Spread')
+plt.xlabel('Hours in HK')
+plt.ylabel('# of people')
+plt.show()
+
 #I>-=-<I>-=-<I>-=-<I>-=-<I>-=-<I>-=-<I>-=-<I>-=-<I>-=-<I>-=-<I>-=-<I>-=-<I>-=-<I>-=-<I>-=-<I>-=-<I>-=-<I>-=-<I>-=-<I>-=-<I
 
 # Load SS data
@@ -49,7 +71,7 @@ print(f"\n{lineBreak}\n\nTail of the SS Dataframe\n")
 print(ss_data.tail())
 
 # Summary of the SS dataframe
-print(f"\n{lineBreak}\n\nSummary of the SS Dataframe\n")             # played SS, SS hours, SS rating, SS sleep
+print(f"\n{lineBreak}\n\nSummary of the SS Dataframe\n")
 print(ss_data.info())
 
 # Statistical Analysis of the SS dataframe
@@ -66,17 +88,24 @@ print(ss_data[ss_data["SS sleep"]=='Greatly Increased'])
 print("\n\n\n\n")
 print(ss_data.groupby('SS rating')['SS hours'])
 
-'''
-ss_data.groupby('SS rating')['SS hours'].mean().plot(kind='scatter')
-plt.title('Hours for Each Rating')
-plt.xlabel('Rating')
-plt.ylabel('Average Hours')
+# Scatterplot of SS Sleep Change vs. SS Hours
+for rating, group in ss_data.groupby('SS sleep #'):
+	plt.scatter([rating]*len(group['SS hours']), group['SS hours'], label=str(rating))
+plt.title('SS Hours for Sleep Change')
+plt.xlabel('Sleep Change')
+plt.ylabel('SS Rating')
 plt.show()
-'''
 
+# Pie chart of SS Ratings
+sizes = ss_data['SS rating'].value_counts().sort_index() / ss_data['SS rating'].value_counts().sum() * 100
+sizes.plot(kind='pie', labels=sizes.index, autopct='%1.1f%%')
+plt.title('SS Rating Spread')
+plt.ylabel('SS Rating')
+plt.show()
 
-df['SS hours'].plot(kind='hist', bins = 10, edgecolor='Black')
-plt.title('Hours for Each Rating')
-plt.xlabel('Rating')
-plt.ylabel('Average Hours')
+# Histogram of spread of SS Hours
+df['SS hours'].plot(kind='hist', bins = 20, edgecolor='Black')
+plt.title('SS Hour Spread')
+plt.xlabel('Hours in SS')
+plt.ylabel('# of people')
 plt.show()
